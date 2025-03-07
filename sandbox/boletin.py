89a9d377bb73a5638e40a8b2fc958c4577da7a0b
minimasoft <minimasoft@proton.me>
2025-03-07 14:34:42 -0300

    use local font

--- sandbox/boletin.py
+++ sandbox/boletin.py
@@ -277,8 +277,16 @@ with open(f"bo{year-2000}{month:02}{day:02}.html",'w') as html_o:
     html_o.write("""<html>
 <head>
 <meta charset="UTF-8">
-<link href='https://fonts.googleapis.com/css?family=Noto Sans Mono' rel='stylesheet'>
 <style>
+@font-face {
+  font-family: 'Noto Sans Mono';
+  font-style: normal;
+  font-weight: 400;
+  font-stretch: 100%;
+  src: url(/NotoSansMonoLatin.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+
 body {
     font-family: 'Noto Sans Mono';
     font-size: 16px;
