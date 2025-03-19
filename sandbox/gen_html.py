fbc197459e7026610020ab95e7f8ac412cd36e10
Esteban Eduardo Torre <minimasoft@proton.me>
2025-03-18 23:20:27 -0300

    fix html layout

--- sandbox/gen_html.py
+++ sandbox/gen_html.py
@@ -59,6 +59,11 @@ td {
   padding: 10px;
 }
 
+.json_data {
+  overflow: scroll;
+  max-width: 1000px;
+}
+
 </style>
 </head>
 <body>
@@ -99,10 +104,10 @@ td {
             html_o.write(f"<details><summary><b>Análisis de bogabot</b></summary>{analysis}</details>\n")
         html_o.write(f"<details><summary><b>Texto original</b></summary>{result['full_text']}</details>\n")
         html_o.write(f"</details>\n</div>\n</td>\n</tr>\n")
-    html_o.write(f"<tr><td><div id=bonus_1><h2><a href=#bonus_1>Bonus 1:</a> designaciones</h2><hr><pre>\n")
+    html_o.write(f"<tr><td><div id=bonus_1 class=json_data><h2><a href=#bonus_1>Bonus 1:</a> designaciones</h2><hr><pre>\n")
     html_o.write(json.dumps(appoint_list, indent=2, ensure_ascii=False))
     html_o.write("\n</pre></div></td></tr>\n")
-    html_o.write(f"<tr><td><div id=bonus_2><h2><a href=#bonus_2>Bonus 2:</a> renuncias</h2><hr><pre>\n")
+    html_o.write(f"<tr><td><div id=bonus_2 class=json_data><h2><a href=#bonus_2>Bonus 2:</a> renuncias</h2><hr><pre>\n")
     html_o.write(json.dumps(resign_list, indent=2, ensure_ascii=False))
     html_o.write("\n</pre></div></td></tr>\n")
     html_o.write('\n</tbody></table></body></html>\n')
